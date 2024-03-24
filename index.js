34f9e791533e3f4f431c6b99fb755d621e31d032
Feryel Khelifi <feryelkhelifi@gmail.com>
2024-03-24 16:54:54 +0100

    Premier commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById('showButton').addEventListener('click', function() {
+    document.getElementById('popupContainer').style.display = 'block';
+  });
+  
+  document.getElementById('closeButton').addEventListener('click', function() {
+    document.getElementById('popupContainer').style.display = 'none';
+  });
+  
